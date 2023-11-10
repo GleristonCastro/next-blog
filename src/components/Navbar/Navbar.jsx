003b108ccfceb0633d8586d4aa991262d50655fa
Gleriston <xpanium@gmail.com>
2023-11-10 00:13:19 -0300

    update: Theme Toggle and AuthLink in the project

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import styles from './navbar.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
+import AuthLinks from '../AuthLinks/AuthLinks'
+import ThemeToggle from '../ThemeToggle/ThemeToggle'
 
 const Navbar = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.social}></div>
-      <div className={styles.logo}></div>
-      <div className={styles.links}></div>
+      <div className={styles.social}>
+        <Image src='/facebook.png' alt='facebook' width={24} height={24} />
+        <Image src='/instagram.png' alt='Instagram' width={24} height={24} />
+        <Image src='/tiktok.png' alt='TikTok' width={24} height={24} />
+        <Image src='/youtube.png' alt='Youtube' width={24} height={24} />
+      </div>
+      <div className={styles.logo}>lamablog</div>
+      <div className={styles.links}>
+        <ThemeToggle />
+        <Link href='/'>Homepage</Link>
+        <Link href='/'>Contact</Link>
+        <Link href='/'>About</Link>
+        <AuthLinks />
+      </div>
     </div>
   )
 }
