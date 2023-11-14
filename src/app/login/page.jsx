a72de1fff2ceb4c4cd3330b3747879f86d57e11f
Gleriston <xpanium@gmail.com>
2023-11-14 00:21:45 -0300

    update login page with useEffect

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,21 +1,23 @@
-'use client';
-import styles from './loginPage.module.css';
-import { useRouter } from 'next/navigation';
-import { signIn, useSession } from 'next-auth/react';
+'use client'
+import { useEffect } from 'react'
+import { signIn, useSession } from 'next-auth/react'
+import styles from './loginPage.module.css'
+import { useRouter } from 'next/navigation'
 
 const LoginPage = () => {
-  const { status } = useSession();
+  const { status } = useSession()
+  const router = useRouter()
 
-  const router = useRouter();
+  useEffect(() => {
+    if (status === 'authenticated') {
+      router.push('/')
+    }
+  }, [status, router])
 
   if (status === 'loading') {
-    return <div className={styles.loading}>Loading...</div>;
+    return <div className={styles.loading}>Loading...</div>
   }
 
-  if (status === 'authenticated') {
-    router.push('/')
-  }
-  
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
@@ -26,7 +28,7 @@ const LoginPage = () => {
         <div className={styles.socialButton}>Sign in with Facebook</div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default LoginPage;
+export default LoginPage
