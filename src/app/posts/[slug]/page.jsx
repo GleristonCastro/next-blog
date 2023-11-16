975efd56d597ba7c8f08d426b7e8d644fb58ee59
Gleriston <xpanium@gmail.com>
2023-11-15 22:42:33 -0300

    update: inclusion of database content mapping

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -1,15 +1,15 @@
-import Menu from '@/components/Menu/Menu';
-import styles from './singlePage.module.css';
-import Image from 'next/image';
-import Comments from '@/components/Comments/Comments';
+import Menu from "@/components/Menu/Menu";
+import styles from "./singlePage.module.css";
+import Image from "next/image";
+import Comments from "@/components/comments/Comments";
 
 const getData = async (slug) => {
-  const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
-    cache: 'no-store',
+  const res = await fetch(`http://localhost:3000/api/posts/${slug}?popular=true`, {
+    cache: "no-store",
   });
 
   if (!res.ok) {
-    throw new Error('Failed');
+    throw new Error("Failed");
   }
 
   return res.json();
@@ -28,7 +28,7 @@ const SinglePage = async ({ params }) => {
           <div className={styles.user}>
             {data?.user?.image && (
               <div className={styles.userImageContainer}>
-                <Image src={data.user.image} alt='' fill className={styles.avatar} />
+                <Image src={data.user.image} alt="" fill className={styles.avatar} />
               </div>
             )}
             <div className={styles.userTextContainer}>
@@ -39,7 +39,7 @@ const SinglePage = async ({ params }) => {
         </div>
         {data?.img && (
           <div className={styles.imageContainer}>
-            <Image src={data.img} alt='' fill className={styles.image} />
+            <Image src={data.img} alt="" fill className={styles.image} />
           </div>
         )}
       </div>
